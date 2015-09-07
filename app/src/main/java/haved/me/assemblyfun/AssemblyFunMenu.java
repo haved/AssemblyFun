cde1ce80923e86e5f1833549a7000347a24a6079
haved <krogstie.havard@gmail.com>
2015-09-07 20:01:41 +0200

    New project. Emulator is not working properly though.

--- app/src/main/java/haved/me/assemblyfun/AssemblyFunMenu.java
+++ app/src/main/java/haved/me/assemblyfun/AssemblyFunMenu.java
@@ -4,21 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 
-public class MainMenuActivity extends AppCompatActivity {
+public class AssemblyFunMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_assembly_fun_menu);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_assembly_fun_menu, menu);
         return true;
     }
 
