95f989725f8212d4f5366a1a46afd165d0d9ab92
haved <krogstie.havard@gmail.com>
2015-09-10 20:14:02 +0200

    Replaced the TaskBrowser menu thing with another activity with buttons in a scrollable list.

--- app/src/main/java/me/havard/assemblyfun/SolveOptions.java
+++ app/src/main/java/me/havard/assemblyfun/SolveOptions.java
@@ -1,33 +1,46 @@
 package me.havard.assemblyfun;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class AssemblyFunMenu extends AppCompatActivity {
+public class SolveOptions extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assembly_fun_menu);
+        setContentView(R.layout.activity_solve_options);
     }
 
-    public void solveTasksButton(View v) {
-        Intent intent = new Intent(this, SolveOptions.class);
-        startActivity(intent);
+    public void onSolveOnlinePressed(View source)
+    {
+
     }
 
-    public void makeTasksButton(View v) {
+    public void onSolveLocalPressed(View source)
+    {
+
+    }
+
+    public void onSolveSolvedPressed(View source)
+    {
+
+    }
+
+    public void onSolvePublishedPressed(View source)
+    {
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_assembly_fun_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_solve_options, menu);
         return true;
     }
 
