b2961c048c3db6d8eac5bf0063faf2941fa09e92
haved <krogstie.havard@gmail.com>
2015-10-11 21:24:13 +0200

    Made the SolutionEditor not crash. Made it load task title and text from the database and set the text views. Also made the text a lot smaller. The keyboard takes up a lot of space. Also built the .apk

--- app/src/main/java/me/havard/assemblyfun/SolutionEditor.java
+++ app/src/main/java/me/havard/assemblyfun/SolutionEditor.java
@@ -1,11 +1,13 @@
 package me.havard.assemblyfun;
 
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,8 +15,13 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabSelectedListener{
+import me.havard.assemblyfun.data.SQLiteCursorLoader;
+import me.havard.assemblyfun.data.tables.LocalTaskTable;
+import me.havard.assemblyfun.data.tables.TaskinfoTable;
+
+public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabSelectedListener, LoaderManager.LoaderCallbacks<Cursor>{
 
     public static final String EXTRAS_SOLUTION_ID = "solutionId";
     public static final String EXTRAS_TASK_ID = "taskId";
@@ -23,9 +30,11 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
     public static final int TASK_PAGE = 0;
     public static final int SOLUTION_PAGE=1;
 
+    private static final int LOADER_ID_TASK_PAGE_CURSOR = 0;
+
     private TabLayout mTabLayout;
     private ViewPager mViewPager;
-    private PagerAdapter mPagerAdapter;
+    private TaskSolutionPagerAdapter mPagerAdapter;
 
     private long mSolutionId;
     private long mTaskId;
@@ -53,6 +62,11 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
         mTaskId = extras.getLong(EXTRAS_TASK_ID, -1);
     }
 
+    public void useTaskPageCursor(Cursor cursor) {
+        EditorTaskFragment fragment = mPagerAdapter.getTaskFragment();
+        fragment.useCursor(cursor);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem m) {
 
@@ -77,7 +91,6 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
-        Log.d("Assembly Fun", "onTabSelected()");
         mViewPager.setCurrentItem(tab.getPosition(), true);
     }
 
@@ -91,26 +104,66 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
 
     }
 
+    private static final String QUERY_TASK_PAGE_CURSOR = String.format("SELECT %s, %s FROM %s, %s WHERE %s.%s=? AND %s.%s=? LIMIT 1", TaskinfoTable.NAME, LocalTaskTable.TASK_TEXT,
+            TaskinfoTable.TABLE_NAME, LocalTaskTable.TABLE_NAME,      TaskinfoTable.TABLE_NAME,TaskinfoTable._ID_TaskIDs,        LocalTaskTable.TABLE_NAME,LocalTaskTable._ID_TaskIDs);
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if(id==LOADER_ID_TASK_PAGE_CURSOR) {
+            return new SQLiteCursorLoader(this, ((AssemblyFunApplication)getApplication()).getDatabase(), QUERY_TASK_PAGE_CURSOR, new String[]{Long.toString(mTaskId), Long.toString(mTaskId)});
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        useTaskPageCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
     class TaskSolutionPagerAdapter extends FragmentPagerAdapter {
+
+        Bundle mFragmentArgs;
+        EditorSolutionFragment mSolutionFragment;
+        EditorTaskFragment mTaskFragment;
 
         public TaskSolutionPagerAdapter(FragmentManager fm) {
             super(fm);
+            mFragmentArgs = new Bundle();
+            mFragmentArgs.putLong(EXTRAS_SOLUTION_ID, mSolutionId);
+            mFragmentArgs.putLong(EXTRAS_TASK_ID, mTaskId);
+        }
+
+        public EditorSolutionFragment getSolutionFragment()
+        {
+            if(mSolutionFragment != null)
+                return mSolutionFragment;
+
+            mSolutionFragment = new EditorSolutionFragment();
+            mSolutionFragment.setArguments(mFragmentArgs);
+            return mSolutionFragment;
+        }
+
+        public EditorTaskFragment getTaskFragment()
+        {
+            if(mTaskFragment != null)
+                return mTaskFragment;
+
+            mTaskFragment = new EditorTaskFragment();
+            mTaskFragment.setArguments(mFragmentArgs);
+            SolutionEditor.this.getLoaderManager().initLoader(LOADER_ID_TASK_PAGE_CURSOR, null, SolutionEditor.this);
+            return mTaskFragment;
         }
 
         @Override
         public Fragment getItem(int position) {
-            Bundle args = new Bundle();
-            args.putLong(EXTRAS_SOLUTION_ID, mSolutionId);
-            args.putLong(EXTRAS_TASK_ID, mTaskId);
-            if(position == SOLUTION_PAGE) {
-                EditorSolutionFragment fragment = new EditorSolutionFragment();
-                fragment.setArguments(args);
-                return fragment;
-            } else if(position == TASK_PAGE) {
-                EditorTaskFragment fragment = new EditorTaskFragment();
-                fragment.setArguments(args);
-                return fragment;
-            }
+            if(position == SOLUTION_PAGE)
+                return getSolutionFragment();
+            else if(position == TASK_PAGE)
+                return getTaskFragment();
 
             Log.e("Assembly Fun", "The TaskSolutionAdapter doesn't have a page with position " + position);
             return null;
@@ -165,6 +218,21 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
                 mTaskId = args.getLong(EXTRAS_TASK_ID, -1);
             }
             return view;
+        }
+
+        public void useCursor(Cursor cursor) {
+            View v = getView();
+            if(v==null){
+                Log.e("Assembly Fun", "The EditorTaskFragment had no view when trying to use the database cursor");
+                return;
+            }
+            cursor.moveToFirst();
+            if(cursor.isAfterLast()){
+                Log.e("Assembly Fun", "The Cursor passed to the EditorTaskFragment didn't have any items. Nothing to fill fields with!");
+                return;
+            }
+            ((TextView)v.findViewById(R.id.solution_editor_task_title)).setText(cursor.getString(cursor.getColumnIndex(TaskinfoTable.NAME)));
+            ((TextView)v.findViewById(R.id.solution_editor_task_desc)).setText(cursor.getString(cursor.getColumnIndex(LocalTaskTable.TASK_TEXT)));
         }
     }
 }
