c25874e27a5eef085e5f147ee2a7785776a84598
haved <krogstie.havard@gmail.com>
2015-10-13 22:01:46 +0200

    Added an empty adapter in the solution editor. (I was busy, okay?) Also built the .apk

--- app/src/main/java/me/havard/assemblyfun/SolutionEditor.java
+++ app/src/main/java/me/havard/assemblyfun/SolutionEditor.java
@@ -3,6 +3,7 @@ package me.havard.assemblyfun;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TextView;
 
 import me.havard.assemblyfun.data.SQLiteCursorLoader;
@@ -216,6 +218,58 @@ public class SolutionEditor extends FragmentActivity implements TabLayout.OnTabS
                 mTaskId = args.getLong(EXTRAS_TASK_ID, -1);
             }
             return view;
+        }
+
+        public class SolutionLineListAdapter implements Adapter {
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
         }
     }
 
