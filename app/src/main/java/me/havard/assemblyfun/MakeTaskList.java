a516a52ab032dc973bbfb3700b20b0e133c73abc
haved <krogstie.havard@gmail.com>
2015-11-25 22:32:57 +0100

    Add a new activity for the make tasks button

---
+++ app/src/main/java/me/havard/assemblyfun/MakeTaskList.java
@@ -0,0 +1,13 @@
+package me.havard.assemblyfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeTaskList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_task_list);
+    }
+}
